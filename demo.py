d0d6a8766fa0639dfe6c3fa98c5537fa85908f0d
aditiagarwal-02 <agarwal.aditi@iitgn.ac.in>
2023-11-15 23:51:53 +0530

    UI changes

--- demo.py
+++ demo.py
@@ -11,24 +11,34 @@ def add_homoskedastic_noise(y, noise_level):
 
 # Function to add heteroskedastic noise to the ground truth
 def add_heteroskedastic_noise(y, noise_factor):
-    return y + np.random.normal(0,noise_factor * X**2, len(y))
+    return y + np.random.normal(0, noise_factor * X**2, len(y))
 
 def rmse(y_pred, y_true):
     return np.sqrt(np.mean((y_pred - y_true) ** 2))
+
 # Streamlit UI
-st.title("MAPIE Regression Demo with Noise")
+st.set_page_config(
+    page_title="MAPIE Regression Demo with Noise",
+    page_icon="✨",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 # Sidebar for user input
 st.sidebar.header("Parameters")
+X_lower = st.sidebar.slider("Dataset X lower limit", min_value=-100.0, max_value=100.0, value=-15.0, step=1.0)
+X_upper = st.sidebar.slider("Dataset X upper limit", min_value=-100.0, max_value=100.0, value=23.0, step=1.0)
 dataset_type = st.sidebar.selectbox("Dataset Type", ["Sine", "Cosine", "Polynomial"])
 noise_type = st.sidebar.selectbox("Noise Type", ["Homoskedastic", "Heteroskedastic"], index=1)
 noise_level = st.sidebar.slider("Noise Level/Factor", min_value=0.0, max_value=1.0, value=0.1, step=0.05)
 
+
+st.title("MAPIE Regression Demo with Noise")
+
 # Generate synthetic data based on the selected dataset type
 np.random.seed(0)
 
-X_lower = st.slider("X lower limit", min_value=-100.0, max_value=100.0, value=-15.0, step=1.0)
-X_upper = st.slider("X upper limit", min_value=-100.0, max_value=100.0, value=23.0, step=1.0)
+
 X = np.linspace(X_lower, X_upper, 100)
 
 if dataset_type == "Sine":
@@ -38,9 +48,7 @@ elif dataset_type == "Cosine":
 elif dataset_type == "Polynomial":
     degree = st.sidebar.slider("Degree", min_value=1.0, max_value=6.0, value=1.0, step=1.0)
     coefficients = np.random.uniform(-10, 10, int(degree))
-    print(coefficients)
-    y_ground_truth =  np.polyval(coefficients, X)
-
+    y_ground_truth = np.polyval(coefficients, X)
 else:
     st.error("Invalid dataset type")
     st.stop()
@@ -56,78 +64,106 @@ model = GradientBoostingRegressor()
 mape_model = MapieRegressor(model)
 mape_model.fit(X.reshape(-1, 1), y_noisy)
 
-st.write("### Dataset")
-
-fig, ax = plt.subplots()
-
-# Plot the ground truth as a line
+# Dataset plot
+st.write("## Dataset")
+fig, ax = plt.subplots(figsize=(10, 6))
 ax.plot(X, y_ground_truth, label='Ground Truth')
-
-# Scatter plot for noisy data
-ax.scatter(X, y_noisy, label='Noisy Data', color='red')
-
-# Set labels and title
+ax.scatter(X, y_noisy, label='Noisy Data', color='red', alpha=0.7)
 ax.set_xlabel('X-axis')
 ax.set_ylabel('Y-axis')
-# ax.set_xlim([-1,1])
-
-
-# Show legend
 ax.legend()
+st.pyplot(fig)
 
-# Display the plot in Streamlit
+# Model prediction plot
+st.write("## Dataset vs. MAPIE Prediction")
+fig, ax = plt.subplots(figsize=(10, 6))
+# ax.plot(X, y_ground_truth, label='Ground Truth')
+ax.plot(X, mape_model.predict(X.reshape(-1, 1)), label='MAPIE Prediction', linestyle='--', color='blue')
+ax.scatter(X, y_noisy, label='Noisy Data', color='red', alpha=0.7)
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.legend()
 st.pyplot(fig)
 
 
-
-# Main content
-st.write(f"### Dataset vs. MAPIE Prediction ({noise_type} Noise)")
-
-# Plotting the ground truth
-st.line_chart({"Noisy Data": y_noisy, "MAPIE Prediction": mape_model.predict(X.reshape(-1, 1))})
+# Model performance and prediction interval
+st.write("## Model Performance and Confidence Interval")
 
 
-# st.write(f"MAPIE Prediction Coverage: {1 - coverage_error(y_noisy.reshape(-1, 1), mape_model.predict(X.reshape(-1, 1)))}")
-confidence_interval = st.slider("Uncertainty Interval", min_value=0.01, max_value=99.99, value=95.00, step=5.00)
-alpha = (100-confidence_interval) / 100
+# Create a table for displaying metrics with custom styling
+
+# Uncertainty interval slider
+confidence_interval = st.slider("Select confidence level", min_value=0.01, max_value=99.99, value=95.00, step=5.00, key="confidence_interval")
+alpha = (100 - confidence_interval) / 100
+
+# Prediction interval plot
 y_pred, y_pis = mape_model.predict(X.reshape(-1, 1), alpha=alpha)
 lower_bound = [subarray[0, 0] for subarray in y_pis]
 upper_bound = [subarray[1, 0] for subarray in y_pis]
-fig, ax = plt.subplots()
 
+fig, ax = plt.subplots(figsize=(10, 6))
 is_within_interval = np.logical_and(y_noisy >= y_pis[:, 0, 0], y_noisy <= y_pis[:, 1, 0])
-
-# Compute the coverage manually
 coverage = np.mean(is_within_interval)
-# Display model performance
-st.write("### Model Performance")
-st.write(f"MAPIE Prediction R2 Score: {mape_model.score(X.reshape(-1, 1), y_noisy)}")
-# st.write(f"MAPIE Prediction MAE: {mape_model.error(X.reshape(-1, 1), y_noisy)}")
-st.write(f"MAPE Prediction RMSE: {rmse(mape_model.predict(X.reshape(-1, 1)), y_noisy)}")
-st.write(f"MAPIE Prediction Coverage: {coverage}")
 
-# Plot the noisy data as a scatter plot
-ax.scatter(X, y_noisy, label='Ground Truth', color='red', alpha=0.5)
+# Applying styling to the table using HTML
+styled_table = f"""
+    <style>
+        table {{
+            font-size: 18px;
+            color: #000066; /* Dark blue color */
+            border-collapse: collapse;
+            width: 50%; /* Adjust the width as needed */
+            margin: auto; /* Center the table */
+            border-spacing: 0; /* Remove spacing between cells */
+            background-color: #e6f7ff; /* Light blue background color */
+        }}
+        th, td {{
+            padding: 20px 30px;
+            text-align: left;
+            border-bottom: 1px solid #ecf0f1; /* Light gray border between rows */
+        }}
+        th {{
+            border-bottom: 2px solid #2980b9; /* Darker blue border for header row */
+        }}
+    </style>
+    <table>
+        <tr>
+            <th>Metric</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>MAPIE Prediction R2 Score</td>
+            <td>{mape_model.score(X.reshape(-1, 1), y_noisy):.4f}</td>
+        </tr>
+        <tr>
+            <td>MAPIE Prediction RMSE</td>
+            <td>{rmse(mape_model.predict(X.reshape(-1, 1)), y_noisy):.4f}</td>
+        </tr>
+        <tr>
+            <td>Coverage</td>
+            <td>{coverage:.4f}</td>
+        </tr>
+    </table>
+"""
 
-# Plot the lower and upper bounds as a shaded area
-ax.fill_between(X, lower_bound, upper_bound, color='gray', alpha=0.3, label='Prediction Interval with ' + str(confidence_interval) + '% Confidence')
+# Display the styled table using markdown
+st.markdown(styled_table, unsafe_allow_html=True)
 
-# Plot the mean prediction
-ax.plot(X, y_pred, label='MAPIE Prediction', color='blue')
 
-# Set labels and title
+# Display the styled table using markdown
+# st.markdown(styled_table, unsafe_allow_html=True)
+
+st.text(" ")
+st.text(" ")
+
+st.text(" ")
+
+
+ax.scatter(X, y_noisy, label='Noisy Data', color='red', alpha=0.7)
+ax.plot(X, y_pred, label='MAPIE Prediction', linestyle='--', color='blue')
+ax.fill_between(X, lower_bound, upper_bound, color='gray', alpha=0.3, label='Confidence Interval')
 ax.set_xlabel('X-axis')
 ax.set_ylabel('Y-axis')
-ax.set_title('MAPIE Prediction and Prediction Interval with ' + str(confidence_interval) + '% Confidence')
-# ax.set_xlim([-1,1])
-# ax.set_ylim([-8,8])
-
-# Show legend
+ax.set_title(f'MAPIE Prediction and Prediction Interval ({confidence_interval}% Confidence)')
 ax.legend()
-
-# Display the plot in Streamlit
 st.pyplot(fig)
-
-
-
-
