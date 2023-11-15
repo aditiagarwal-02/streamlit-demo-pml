53df4ee965cb3c414acf946f4e2ed62f991e0afb
aditiagarwal-02 <agarwal.aditi@iitgn.ac.in>
2023-11-15 18:24:09 +0530

    visual changes

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.ensemble import GradientBoostingRegressor
 from mapie.regression import MapieRegressor
-from sklearn.datasets import make_s_curve
+from sklearn.metrics import coverage_error
 
 # Function to add homoskedastic noise to the ground truth
 def add_homoskedastic_noise(y, noise_level):
@@ -52,22 +53,73 @@ model = GradientBoostingRegressor()
 mape_model = MapieRegressor(model)
 mape_model.fit(X.reshape(-1, 1), y_noisy)
 
+st.write("### Dataset")
+
+fig, ax = plt.subplots()
+
+# Plot the ground truth as a line
+ax.plot(X, y_ground_truth, label='Ground Truth')
+
+# Scatter plot for noisy data
+ax.scatter(X, y_noisy, label='Noisy Data', color='red')
+
+# Set labels and title
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_xlim([-1,1])
+
+
+# Show legend
+ax.legend()
+
+# Display the plot in Streamlit
+st.pyplot(fig)
+
+
+
 # Main content
 st.write(f"### Ground Truth vs. Noisy Data vs. MAPIE Prediction ({noise_type} Noise)")
 
 # Plotting the ground truth
 st.line_chart({"Ground Truth": y_ground_truth, "Noisy Data": y_noisy, "MAPIE Prediction": mape_model.predict(X.reshape(-1, 1))})
-st.scatter_chart({"Ground Truth": y_ground_truth, "Noisy Data": y_noisy, "MAPIE Prediction": mape_model.predict(X.reshape(-1, 1))})
 
 # Display model performance
 st.write("### Model Performance")
 st.write(f"MAPIE Prediction R2 Score: {mape_model.score(X.reshape(-1, 1), y_noisy)}")
 # st.write(f"MAPIE Prediction MAE: {mape_model.error(X.reshape(-1, 1), y_noisy)}")
 st.write(f"MAPE Prediction RMSE: {rmse(mape_model.predict(X.reshape(-1, 1)), y_noisy)}")
-
+# st.write(f"MAPIE Prediction Coverage: {coverage_error(y_noisy.reshape(-1,1), mape_model.predict(X.reshape(-1, 1)))}")
 # Display uncertainty intervals
-st.write("### Prediction Uncertainty Intervals (90%)")
-y_pred, y_pis = mape_model.predict(X.reshape(-1, 1), alpha=0.1)
+# noise_level = st.sidebar.slider("Noise Level/Factor", min_value=0.0, max_value=1.0, value=0.1, step=0.05)
+confidence_interval = st.slider("Uncertainty Interval", min_value=0.01, max_value=99.99, value=95.00, step=5.00)
+alpha = (100-confidence_interval) / 100
+y_pred, y_pis = mape_model.predict(X.reshape(-1, 1), alpha=alpha)
 lower_bound = [subarray[0, 0] for subarray in y_pis]
 upper_bound = [subarray[1, 0] for subarray in y_pis]
-st.line_chart({"Lower Bound": lower_bound, "Upper Bound": upper_bound, "MAPIE Prediction": mape_model.predict(X.reshape(-1, 1))})
+fig, ax = plt.subplots()
+
+# Plot the noisy data as a scatter plot
+ax.scatter(X, y_noisy, label='Ground Truth', color='red', alpha=0.5)
+
+# Plot the lower and upper bounds as a shaded area
+ax.fill_between(X, lower_bound, upper_bound, color='gray', alpha=0.3, label='Prediction Interval with ' + str(confidence_interval) + '% Confidence')
+
+# Plot the mean prediction
+ax.plot(X, y_pred, label='MAPIE Prediction', color='blue')
+
+# Set labels and title
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_title('MAPIE Prediction and Prediction Interval with ' + str(confidence_interval) + '% Confidence')
+ax.set_xlim([-1,1])
+ax.set_ylim([-2,2])
+
+# Show legend
+ax.legend()
+
+# Display the plot in Streamlit
+st.pyplot(fig)
+
+
+
+
